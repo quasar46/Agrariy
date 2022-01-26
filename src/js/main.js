bf19acf0d47ce04a7786808f3714ec7e06f8e191
lock <falsivates@gmail.com>
2022-01-26 19:14:30 +0300

    26.01.22

--- src/js/main.js
+++ src/js/main.js
@@ -7,33 +7,83 @@ $(document).ready(function () {
     dots: true,
   });
 
-  $(".news-inner__slider").slick({
+  $(".news-index__slider").slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     dots: true,
+    responsive: [{
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 361,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+        }
+      }
+    ]
   });
 
   $(".partners-inner__slider").slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    dots: true,
+    dots: false,
+    responsive: [{
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          arrows: true,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 361,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+          dots: true,
+        }
+      }
+    ]
   });
 
-  // $(".header__burger").on("click", function () {
-  //   $(".aside").toggleClass("active");
-  // });
-  $('#burger').on('click', function() {
-    $(this).toggleClass('active');
-    $('.main-menu').toggleClass('show');
+  $(".about__stats").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    responsive: [{
+      breakpoint: 361,
+      settings: {
+        slidesToShow: 1,
+        dots: true,
+      }
+    }]
+  })
+
+  $('#burger').on('click', function () {
+    if (window.innerWidth > 360) {
+      $('body').toggleClass('hidden');
+      $(this).toggleClass('active');
+      $('.main-menu').toggleClass('show');
+    } else {
+      $('body').toggleClass('hidden');
+      $(this).toggleClass('active');
+      $('.header__nav > ul').toggleClass('show');
+    }
   })
 
   $(".aside").hover(
-    function() {
+    function () {
       $(this).addClass("show")
     },
-    function() {
+    function () {
       $(this).removeClass("show")
     }
   );
@@ -42,13 +92,13 @@ $(document).ready(function () {
     $(".phone-input").mask("+7(999) 999-99-99");
   });
 
-  $(".hero__search").on("click", function () {
+  $(".search").on("click", function () {
     $(this).addClass("active");
   });
   // сварачиваем поиск по клику на пустое место
   $(document).mouseup(function (e) {
-    if ($(".hero__search").has(e.target).length === 0) {
-      $(".hero__search").removeClass("active");
+    if ($(".search").has(e.target).length === 0) {
+      $(".search").removeClass("active");
     }
   });
   $("#buttonCall").on("click", function () {
@@ -63,4 +113,50 @@ $(document).ready(function () {
   $(".cross").on("click", function () {
     $(".modal").removeClass("show");
   });
+
+  if (window.innerWidth < 361) {
+    $('.inner-block__header a').html('Все');
+  }
+
+  $('#footerNav').on('click', function () {
+    $(this).toggleClass('active');
+    $('.footer__nav nav').toggleClass('active');
+  })
+
+  // tabs
+  $(function () {
+    $('.tabs__header').on('click', 'li:not(.active)', function () {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+  });
+
+  // отоброжание количества прикрепленных файлов
+  let inputs = document.querySelectorAll('.input__file');
+  Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling,
+      labelVal = label.querySelector('.input__file-button-text').innerText;
+
+    input.addEventListener('change', function (e) {
+      let countFiles = '';
+      if (this.files && this.files.length >= 1)
+        countFiles = this.files.length;
+
+      if (countFiles)
+        label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+      else
+        label.querySelector('.input__file-button-text').innerText = labelVal;
+    });
+  });
+
+  //- инициализация fancybox
+  $('[data-fancybox="video-gallery"]').fancybox({
+    // Options will go here
+  });
+
+  $('.toggle').on('click', function () {
+    $(this).next().toggle();
+    $(this).toggleClass('active');
+  })
 });
